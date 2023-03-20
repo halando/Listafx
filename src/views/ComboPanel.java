886ede3780ab8b820601d77a24561b55365644bb
Andó Attila <andoattila698@gmail.com>
2023-03-20 16:39:32 +0100

    Maria Terezia

---
+++ src/views/ComboPanel.java
@@ -0,0 +1,13 @@
+package views;
+
+import javafx.scene.control.ComboBox;
+
+public class ComboPanel extends ComboBox<String>{
+
+    public ComboPanel() {
+        this.getItems().add("déli");
+        this.getItems().add("hazai");
+        
+    }
+    
+}
